53057749b5e01e4b99907ec2a51fd12c426e7478
UryGDN <92891778+UryGDN@users.noreply.github.com>
2021-11-29 21:28:14 -0300

    Add files via upload

---
+++ Site/JS/Carrossel.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let slides = document.getElementsByClassName("fade");
+
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+
+    dots[slideIndex-1].className += " active";
+
+}
